3cf30b540524cf7221f000f5f7106553fd7ae54b
Simon Minton <simon@foyerlabs.com>
2021-03-08 21:34:40 +0000

    share your story

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -7,7 +7,7 @@
             <a class="text-base xl:text-lg text-ultra-light-grey px-2 xl:px-4" href="/companies">Company Reviews</a>
             <a class="text-base xl:text-lg text-ultra-light-grey px-2 xl:px-4" href="/share-your-story#how-it-works">How it Works</a>
             <a class="text-base xl:text-lg text-ultra-light-grey px-2 xl:px-4" href="/about-us">About Us</a>
-            <a class="ml-8 rounded-md bg-light-green px-1 xl:px-3 py-4 text-dark-grey font-bold text-sm xl:text-base" href="your-story">Write A Review</a>
+            <a class="ml-8 rounded-md bg-light-green px-1 xl:px-3 py-4 text-dark-grey font-bold text-sm xl:text-base" href="your-story">Share Your Story</a>
         </div>
     </div>
 </div>
